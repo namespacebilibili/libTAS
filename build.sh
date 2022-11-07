86703ae1bebd78326f85bc90c32cb45c78987635
Zhancun Mu <91790440+namespacebilibili@users.noreply.github.com>
2022-11-07 15:19:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ aclocal
 autoconf
 autoheader
 automake --add-missing
-CXXFLAGS="-O2 -std=gnu11 -g -Wall -pedantic" ./configure "$@"
+CXXFLAGS="-O2 -std=gnu++11 -g -Wall -pedantic" ./configure "$@"
 make -j4
